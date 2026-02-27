eb02a5b8303e84b2355f981d9f192532c9eeeaf7
allurjj <allurjj@users.noreply.github.com>
2026-02-26 21:07:36 -0600

    chore: Bump version to 1.1.14

--- radio_monitor/__init__.py
+++ radio_monitor/__init__.py
@@ -80,7 +80,7 @@ python -m radio_monitor.cli --plex-playlist "Radio Hits" --days 7 --limit 50
 Version: 1.1.9
 """
 
-__version__ = "1.1.13"
+__version__ = "1.1.14"
 __author__ = "Radio Monitor Team"
 __github_url__ = "https://github.com/allurjj/radio-monitor"
 
