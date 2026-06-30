6810298c138a19c5353050ce636671aea6ad106f
allurjj <allurjj@users.noreply.github.com>
2026-06-29 21:51:27 -0500

    chore: remove build artifacts from git tracking

--- dist/pyinstaller/Radio Monitor/_internal/radio_monitor/database/migrations/rollback_v18_to_v17.sql
+++
@@ -1,7 +0,0 @@
--- Rollback v18 to v17: Remove retry_match_succeeded column from plex_match_failures
-
--- Remove retry_match_succeeded column
-ALTER TABLE plex_match_failures DROP COLUMN retry_match_succeeded;
-
--- Update schema version
-UPDATE schema_version SET version = 17;
